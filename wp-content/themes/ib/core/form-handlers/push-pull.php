31e006ea4c0d416de77cd0ef21b90ada890236cc
abubakar3rd <abubakar3rd@gmail.com>
2023-04-26 04:07:08 +0000

    Online Application database connection

--- wp-content/themes/ib/core/form-handlers/push-pull.php
+++ wp-content/themes/ib/core/form-handlers/push-pull.php
@@ -4,9 +4,9 @@
     use PHPMailer\PHPMailer\Exception;
     
     // require_once get_template_directory() . "/core/form-handlers/load.php";
-    // require_once get_template_directory() . "/core/PHPMailer/src/PHPMailer.php";
-    // require_once get_template_directory() . "/core/PHPMailer/src/SMTP.php";
-    // require_once get_template_directory() . "/core/PHPMailer/src/Exception.php";
+    require_once get_template_directory() . "/core/PHPMailer/src/PHPMailer.php";
+    require_once get_template_directory() . "/core/PHPMailer/src/SMTP.php";
+    require_once get_template_directory() . "/core/PHPMailer/src/Exception.php";
 
     if (isset($_POST["internet-banking"])) {
         $ApplicantTitle                 = $_POST["title"];
