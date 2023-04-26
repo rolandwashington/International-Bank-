70dae1f9e319d33fe06363ab94f4c4e69714b713
abubakar3rd <abubakar3rd@gmail.com>
2023-04-26 04:00:27 +0000

    Online Application database connection

--- wp-content/themes/ib/core/form-handlers/push-pull.php
+++ wp-content/themes/ib/core/form-handlers/push-pull.php
@@ -3,7 +3,7 @@
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
     
-    require_once get_template_directory() . "/core/form-handlers/load.php";
+    // require_once get_template_directory() . "/core/form-handlers/load.php";
     require_once get_template_directory() . "/core/PHPMailer/src/PHPMailer.php";
     require_once get_template_directory() . "/core/PHPMailer/src/SMTP.php";
     require_once get_template_directory() . "/core/PHPMailer/src/Exception.php";
