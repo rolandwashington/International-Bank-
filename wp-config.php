7744aaf39b6d51e361297a5606f053d6c5698ee9
abubakar3rd <abubakar3rd@gmail.com>
2023-04-26 03:25:48 +0000

    Online Application database connection

--- wp-config.php
+++ wp-config.php
@@ -45,6 +45,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+
+require_once('database.php');
+
 /**#@+
  * Authentication unique keys and salts.
  *
