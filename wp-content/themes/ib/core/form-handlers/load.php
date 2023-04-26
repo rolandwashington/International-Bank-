d8f842f25560ddd8f79b7e75018448472cf16842
abubakar3rd <abubakar3rd@gmail.com>
2023-04-26 04:21:37 +0000

    Online Application database connection

--- wp-content/themes/ib/core/form-handlers/load.php
+++ wp-content/themes/ib/core/form-handlers/load.php
@@ -1,11 +1,9 @@
 <?php 
     require_once get_template_directory() . "/core/form-handlers/database.php";
-    // require_once get_template_directory() . "/core/classes/User.php";
+    require_once get_template_directory() . "/core/classes/User.php";
 
-	// $loadFromUser = new User($pdo);
+	$loadFromUser = new User($pdo);
   
-    require_once get_template_directory() . "/core/form-handlers/database.php";
-
     $SMTPEmailAddress   = 'admin@international-bank.thekreativezone.com';
     $SMTPEmailPassword  = 'p$TpsM!!BTFkzx8Q&pJK!';
     $SMTPHost           = 'box2064.bluehost.com';
