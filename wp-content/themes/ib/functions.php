9232c47cdd7121442c226944654d52f62dcac14a
Abu-Bakar Abdullah Kanneh <abubakar3rd@gmail.com>
2023-05-26 13:29:20 +0000

    Done with E-Statement, form data can be sent to database and email

--- wp-content/themes/ib/functions.php
+++ wp-content/themes/ib/functions.php
@@ -121,7 +121,7 @@ function create_custom_tables() {
         PRIMARY KEY  (id)
     ) $charset_collate;";
 
-    // SMS ALERT
+    // MOBILE BANKING
     $mobile_banking = $wpdb->prefix . 'ib_mobile_banking';
 
     $mobile_banking_table = "CREATE TABLE $mobile_banking (
@@ -151,6 +151,35 @@ function create_custom_tables() {
     ) $charset_collate;";
 
 
+    // MOBILE BANKING
+    $e_statement = $wpdb->prefix . 'ib_e_statement';
+
+    $e_statement_table = "CREATE TABLE $e_statement (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        HaveAnAccountWithIB VARCHAR(3) NOT NULL,
+        ExistingAccountNumber VARCHAR(50) NOT NULL,
+        AccountHolderTitle VARCHAR(10) NOT NULL,
+        AccountHolderFirstName VARCHAR(50) NOT NULL,
+        AccountHolderMiddleName VARCHAR(50),
+        AccountHolderLastName VARCHAR(50) NOT NULL,
+        AccountHolderPhoneNumberOne VARCHAR(50) NOT NULL,
+        AccountHolderPhoneNumberTwo VARCHAR(50),
+        AccountHolderEmail VARCHAR(50) NOT NULL,
+        
+        -- ADDITIONAL SERVICES
+        IsAdditionalEServices VARCHAR(3) NOT NULL,
+        SMSBanking VARCHAR(20) NOT NULL,
+        InternetBanking VARCHAR(20) NOT NULL,
+        PushAndPull VARCHAR(20) NOT NULL,
+        Ealerts VARCHAR(20) NOT NULL,
+        EmailInstructions VARCHAR(20) NOT NULL,
+        MobileBanking VARCHAR(10) NOT NULL,
+        Mastercard VARCHAR(10) NOT NULL,
+        ATM VARCHAR(20) NOT NULL,
+        IsAgreedToTerms VARCHAR(3) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
 
 
 
@@ -160,6 +189,7 @@ function create_custom_tables() {
     dbDelta( $online_banking_table );
     dbDelta( $sms_alert_table );
     dbDelta( $mobile_banking_table );
+    dbDelta( $e_statement_table );
 }
 
 add_action( 'after_setup_theme', 'create_custom_tables' );
@@ -282,28 +312,3 @@ add_action( 'after_setup_theme', 'create_custom_tables' );
 
 // add_action( 'wp_loaded', 'create_custom_tables' );
   
-
-// $DATABASE_TABLES = [
-
-//     'CREATE TABLE Mobile_Banking (
-//         -- PERSONAL INFO
-//         AccountID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//         HaveAnAccountWithIB VARCHAR(3) NOT NULL,
-//         AccountHolderTitle VARCHAR(10) NOT NULL,
-//         AccountHolderFirstName VARCHAR(50) NOT NULL,
-//         AccountHolderMiddleName VARCHAR(50) DEFAULT NULL,
-//         AccountHolderLastName VARCHAR(50) NOT NULL,
-//         AccountHolderPhoneNumberOne VARCHAR(50) NOT NULL,
-//         AccountHolderPhoneNumberTwo VARCHAR(50) DEFAULT NULL,
-//         AccountHolderEmail VARCHAR(50) NOT NULL,
-        
-//         -- ADDITIONAL SERVICES
-//         IsAdditionalEServices VARCHAR(3) NOT NULL,
-//         ATM VARCHAR(20) NOT NULL,
-//         SMSBanking VARCHAR(20) NOT NULL,
-//         PushAndPull VARCHAR(20) NOT NULL,
-//         Ealerts VARCHAR(20) NOT NULL,
-//         EmailInstructions VARCHAR(20) NOT NULL,
-//         IsAgreedToTerms VARCHAR(3) NOT NULL
-//     ) ENGINE = INNODB DEFAULT CHARSET = latin1;',
-// ]; 
