e5fcc68b69b441a272fdcf16961ae10918db20e3
Abu-Bakar Abdullah Kanneh <abubakar3rd@gmail.com>
2024-04-22 13:47:24 +0000

    Deploying Google Analytics

--- wp-content/themes/ib/footer.php
+++ wp-content/themes/ib/footer.php
@@ -182,5 +182,11 @@
             });
         });
     </script>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5NDMDLC" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
--- wp-content/themes/ib/header.php
+++ wp-content/themes/ib/header.php
@@ -47,6 +47,14 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T5NDMDLC');</script>
+    <!-- End Google Tag Manager -->
+
 </head>
 	<div id="page" class="site">
 
