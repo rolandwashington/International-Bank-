022112e31f6269e6e0afa84bb155b8e87fd3ecc8
Abu-Bakar Abdullah Kanneh <abubakar3rd@gmail.com>
2024-08-17 16:11:46 +0000

    Change Annoucement to Vacancy and made blog the default for publications

--- wp-content/themes/ib/page-blog.php
+++ wp-content/themes/ib/page-blog.php
@@ -36,7 +36,7 @@
                                         <div class="entry-media">
                                             <?php if ( has_post_thumbnail() ) : ?>
                                                 <a href="<?php the_permalink(); ?>">
-                                                    <?php the_post_thumbnail( 'medium' ); ?>
+                                                    <?php the_post_thumbnail( 'large' ); ?>
                                                 </a>
                                             <?php endif; ?>
                                         </div>
